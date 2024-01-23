759647be278f2ccbbec0df67391240c896324c18
Akshay <aksrahangdale@gmail.com>
2024-01-23 15:26:39 +0530

    initialized project setup

---
+++ src/app/layout.tsx
@@ -0,0 +1,36 @@
+import Notification from "@/components/Notification";
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+import AuthProvider from "@/components/AuthProvider";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Z9 Restaurant",
+  description: "Best Food in Town",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <AuthProvider>
+        <div>
+          <Notification />
+          <Navbar />
+          {children}
+          <Footer />
+        </div>
+        </AuthProvider>
+      </body>
+
+    </html>
+  );
+}
