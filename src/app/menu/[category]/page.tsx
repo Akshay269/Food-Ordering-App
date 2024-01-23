a1c5583321b8c9f1dc0e977c866e50addb8c78e7
Akshay <aksrahangdale@gmail.com>
2024-01-23 16:25:47 +0530

    added api routes of category and product

--- src/app/menu/[category]/page.tsx
+++ src/app/menu/[category]/page.tsx
@@ -1,9 +1,28 @@
 import { pizzas } from "@/data";
+import { ProductType } from "@/types/type";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
-const CategoryPage = () => {
+const getData = async (category:string)=>{
+  const res = await fetch(`http://localhost:3000/api/products?cat=${category}`,{
+    cache:"no-store"
+  })
+
+  if(!res.ok){
+    throw new Error("Failed!");
+    
+  }
+
+  return res.json()
+}
+
+type Props = {
+  params:{category:string}
+}
+
+const CategoryPage = async({params}:Props) => {
+  const products:ProductType[] = await getData(params.category)
   return (
     <div className="flex flex-wrap text-red-500">
       {pizzas.map((item) => (
