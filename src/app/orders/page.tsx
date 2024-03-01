519e08ed5e5f6f54e69d6b6e9a6e20efed831ed3
Akshay269 <aksrahangdale@gmail.com>
2024-03-02 00:34:06 +0530

    Order and delete button

--- src/app/orders/page.tsx
+++ src/app/orders/page.tsx
@@ -1,29 +1,57 @@
 "use client";
-import { OrderType } from "@/types/type";
-import { useQuery } from "@tanstack/react-query";
-import { useSession } from "next-auth/react";
-import { useRouter } from "next/router";
-import Image from "next/image";
-import {toast} from "react-toastify";
 
+import { OrderType } from "@/types/type";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { useSession } from "next-auth/react";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
 import React from "react";
-//react query
+import { toast } from "react-toastify";
 
 const OrdersPage = () => {
   const { data: session, status } = useSession();
+
   const router = useRouter();
 
   if (status === "unauthenticated") {
     router.push("/");
   }
 
-  const { isPending, error, data } = useQuery({
+  const { isLoading, error, data } = useQuery({
     queryKey: ["orders"],
     queryFn: () =>
       fetch("http://localhost:3000/api/orders").then((res) => res.json()),
   });
 
-  if (isPending || status === "loading") return "Loading...";
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation({
+    mutationFn: ({ id, status }: { id: string; status: string }) => {
+      return fetch(`http://localhost:3000/api/orders/${id}`, {
+        method:"PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(status),
+      });
+    },
+    onSuccess() {
+      queryClient.invalidateQueries({ queryKey: ["orders"] });
+    },
+  });
+
+  const handleUpdate = (e: React.FormEvent<HTMLFormElement>, id: string) => {
+    e.preventDefault();
+    const form = e.target as HTMLFormElement;
+    const input = form.elements[0] as HTMLInputElement;
+    const status = input.value;
+
+    mutation.mutate({ id, status });
+    toast.success("The order status has been changed!")
+  };
+
+  if (isLoading || status === "loading") return "Loading...";
+
   return (
     <div className="p-4 lg:px-20 xl:px-40">
       <table className="w-full border-separate border-spacing-3">
